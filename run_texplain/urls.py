8dc4ab2b6925accc48ce811a7ef2b12411609511
ZachJHansen <zachhansen@unomaha.edu>
2024-11-07 10:22:15 -0600

    fixing home url

--- run_texplain/urls.py
+++ run_texplain/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.home, name='editor-home'),
-    path("home/", views.get_info),
+    path('', views.get_info, name='editor-home'),
+    #path("home/", views.get_info),
     path("output/", views.output)
 ]
